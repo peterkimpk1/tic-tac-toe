a20f89f8e89141ca5ad7d0afc58d1dd90f07bae0
Peter Kim <peterkim.pk1@gmail.com>
2024-04-16 15:57:08 -0400

    Added query selectors and player objects

---
+++ main.js
@@ -0,0 +1,27 @@
+var playerOneWins = document.querySelector('.player-one-wins');
+var playerTwoWins = document.querySelector('.player-two-wins');
+var playerOneToken = document.querySelector('.player-one-icon');
+var playerTwoToken = document.querySelector('.player-two-icon')
+var gameStatus = document.querySelector('.game-status');
+var gameBoard = document.querySelectorAll('.grid-item');
+var playerOne = {
+    id: 0,
+    token: playerOneToken.innerText,
+    wins: 0
+};
+var playerTwo = {
+    id: 1,
+    token: playerTwoToken.innerText,
+    wins: 0
+};
+
+function increaseWins() {
+
+}
+
+function determineWin() {
+}
+
+function determineDraw() {
+
+}

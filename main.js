34666936097b7d3bd187629af61e3b069d88ff97
Peter Kim <peterkim.pk1@gmail.com>
2024-04-16 17:20:46 -0400

    Token is updated for player turn

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ var playerOne = {
     id: 0,
     token: playerOneToken.innerText,
     wins: 0,
-    turn: true
+    turn: 0
 };
 var playerTwo = {
     id: 1,
     token: playerTwoToken.innerText,
     wins: 0,
-    turn: false
+    turn: 0
 };
 var players = [playerOne, playerTwo]
+var playerTurn;
 function increaseWins() {
 
 }
@@ -35,19 +36,21 @@ function determineDraw() {
 function determineTurn() {
     var numberOfPlacedPieces = gameBoard.length;
     if (!numberOfPlacedPieces || numberOfPlacedPieces % 2 === 0) {
+        playerTurn = 1;
         gameStatus.innerText = `It's ${players[1].token}'s turn`
     }
     else if (!numberOfPlacedPieces % 2 === 0) {
+        playerTurn = 0;
         gameStatus.innerText = `It's ${players[0].token}'s turn`
     }
 }   
 
 function placeToken(e) {
     e.preventDefault();
-    determineTurn();
-    if (e.target.className === 'grid-item') {
+    if (e.target.className === 'grid-item' && !gameBoard.includes(e.target.id)) {
+      determineTurn();
       gameBoard.push(e.target.id)
-      e.target.innerText = playerOne.token;
+      e.target.innerText = players[playerTurn].token;
     }
 }
 determineTurn();

1f49bfe2fd202bc2d910038ec39b53301304ee15
Peter Kim <peterkim.pk1@gmail.com>
2024-04-16 18:03:48 -0400

    Created logic to check if a player has won. Created function for determining draw. Created clear function to be invoked whenever a player has won or there is a draw

--- main.js
+++ main.js
@@ -6,41 +6,57 @@ var gameStatus = document.querySelector('.game-status');
 var gameBoardContainer = document.querySelector('.grid-container');
 var gameSquares = document.querySelectorAll('.grid-item');
 var gameBoard = [];
-
-gameBoardContainer.addEventListener('click',placeToken)
 var playerOne = {
     id: 0,
     token: playerOneToken.innerText,
     wins: 0,
-    turn: 0
+    placedPieces: []
 };
 var playerTwo = {
     id: 1,
     token: playerTwoToken.innerText,
     wins: 0,
-    turn: 0
+    placedPieces: []
 };
 var players = [playerOne, playerTwo]
 var playerTurn;
-function increaseWins() {
 
-}
+gameBoardContainer.addEventListener('click',placeToken)
 
 function determineWin() {
+    var playerPieces = players[playerTurn].placedPieces
+    if((playerPieces.includes('one') && playerPieces.includes('two') && playerPieces.includes('three')) || (playerPieces.includes('four') && playerPieces.includes('five') && playerPieces.includes('six')) || (playerPieces.includes('seven') && playerPieces.includes('eight') && playerPieces.includes('nine')) || (playerPieces.includes('one') && playerPieces.includes('four') && playerPieces.includes('seven')) || (playerPieces.includes('two') && playerPieces.includes('five') && playerPieces.includes('eight')) || (playerPieces.includes('three') && playerPieces.includes('six') && playerPieces.includes('nine')) || (playerPieces.includes('one') && playerPieces.includes('five') && playerPieces.includes('nine')) || (playerPieces.includes('three') && playerPieces.includes('five') && playerPieces.includes('seven'))) {
+        players[playerTurn].wins += 1;
+        playerOneWins.innerText= `${playerOne.wins} wins`
+        playerTwoWins.innerText = `${playerTwo.wins} wins`
+        clearBoard();
+    }
+}
 
+function clearBoard () {
+    gameBoard = [];
+    players[0].placedPieces = [];
+    players[1].placedPieces = [];
+    for (var i = 0; i < gameSquares.length; i++) {
+        gameSquares[i].innerText = ""
+    }
 }
 
 function determineDraw() {
-
+    if (gameBoard.length === 9) {
+        gameStatus.innerText = 'The game is a draw.'
+        clearBoard()
+    }
 }
+
 function determineTurn() {
     var numberOfPlacedPieces = gameBoard.length;
-    if (!numberOfPlacedPieces || numberOfPlacedPieces % 2 === 0) {
-        playerTurn = 1;
+    if (numberOfPlacedPieces % 2 === 0) {
+        playerTurn = 0;
         gameStatus.innerText = `It's ${players[1].token}'s turn`
     }
     else if (!numberOfPlacedPieces % 2 === 0) {
-        playerTurn = 0;
+        playerTurn = 1;
         gameStatus.innerText = `It's ${players[0].token}'s turn`
     }
 }   
@@ -50,7 +66,9 @@ function placeToken(e) {
     if (e.target.className === 'grid-item' && !gameBoard.includes(e.target.id)) {
       determineTurn();
       gameBoard.push(e.target.id)
+      players[playerTurn].placedPieces.push(e.target.id)
       e.target.innerText = players[playerTurn].token;
     }
+    determineWin();
+    determineDraw();
 }
-determineTurn();
